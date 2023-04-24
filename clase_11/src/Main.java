e0fc9c0960d14f546f6780f304cd144276caadd1
Sergio Medina <hola@sergiomedina.io>
2023-04-24 10:31:08 -0300

    refactor: move Player to Tournament

--- clase_11/src/Main.java
+++ clase_11/src/Main.java
@@ -1,43 +1,6 @@
+import Tournament.Player;
+
 import java.util.ArrayList;
-
-class Player {
-    private int number;
-    private String name;
-    private String position;
-    private int goalCounter;
-
-    public int getNumber() {
-        return number;
-    }
-
-    public void setNumber(int number) {
-        this.number = number;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPosition() {
-        return position;
-    }
-
-    public void setPosition(String position) {
-        this.position = position;
-    }
-
-    public int getGoalCounter() {
-        return goalCounter;
-    }
-
-    public void setGoalCounter(int goalCounter) {
-        this.goalCounter = goalCounter;
-    }
-}
 
 class Team {
     private String name;
@@ -60,7 +23,7 @@ class Team {
     }
 
     public void addPlayer(Player player) {
-        
+
     }
 
     public void removePlayer(Player player) {
