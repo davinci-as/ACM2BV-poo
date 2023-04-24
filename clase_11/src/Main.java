8988fd3a309a9f010aa71a16db5f942bed53e09b
Sergio Medina <hola@sergiomedina.io>
2023-04-24 10:06:58 -0300

    feat: add Team class

--- clase_11/src/Main.java
+++ clase_11/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 class Player {
     private int number;
     private String name;
@@ -34,6 +36,35 @@ class Player {
 
     public void setGoalCounter(int goalCounter) {
         this.goalCounter = goalCounter;
+    }
+}
+
+class Team {
+    private String name;
+    private ArrayList<Player> players;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(ArrayList<Player> players) {
+        this.players = players;
+    }
+
+    public void addPlayer(Player player) {
+        
+    }
+
+    public void removePlayer(Player player) {
+
     }
 }
 
