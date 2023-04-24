34ac07ee8e306eacfe392e0e476f077e12f1a76d
Sergio Medina <hola@sergiomedina.io>
2023-04-24 10:33:08 -0300

    refactor: move Team class to Tournament

--- clase_11/src/Main.java
+++ clase_11/src/Main.java
@@ -1,37 +1,3 @@
-import Tournament.Player;
-
-import java.util.ArrayList;
-
-class Team {
-    private String name;
-    private ArrayList<Player> players;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public ArrayList<Player> getPlayers() {
-        return players;
-    }
-
-    public void setPlayers(ArrayList<Player> players) {
-        this.players = players;
-    }
-
-    public void addPlayer(Player player) {
-
-    }
-
-    public void removePlayer(Player player) {
-
-    }
-}
-
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
---
+++ clase_11/src/Tournament/Team.java
@@ -0,0 +1,32 @@
+package Tournament;
+
+import java.util.ArrayList;
+
+public class Team {
+    private String name;
+    private ArrayList<Player> players;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(ArrayList<Player> players) {
+        this.players = players;
+    }
+
+    public void addPlayer(Player player) {
+
+    }
+
+    public void removePlayer(Player player) {
+
+    }
+}
